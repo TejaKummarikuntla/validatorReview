6a7b0a648d8989239874add3bc56c78caa9616ec
TejaKummarikuntla <teja.kummarikuntla@gmail.com>
2018-09-16 11:29:14 +0530

    LocError

--- app/src/main/java/com/coffee/qrcodescanner/Camera/cameraActivity.java
+++ app/src/main/java/com/coffee/qrcodescanner/Camera/cameraActivity.java
@@ -1,9 +1,21 @@
 package com.coffee.qrcodescanner.Camera;
 
-
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
@@ -17,17 +29,18 @@ import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.location.Address;
 import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.media.Image;
 import android.media.ImageReader;
 import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -40,6 +53,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.coffee.qrcodescanner.MainActivity;
 import com.coffee.qrcodescanner.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -61,11 +75,10 @@ import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
-public class cameraActivity extends AppCompatActivity {
+public class cameraActivity extends AppCompatActivity implements LocationListener {
 
 
     TextView textView;
-    LocationManager locationManager;
     double longitude = 0;
     double latitube = 0;
     private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
@@ -77,11 +90,10 @@ public class cameraActivity extends AppCompatActivity {
 
     protected Location mLastLocation;
 
-    private String mLatitudeLabel;
-    private String mLongitudeLabel;
     private TextView mLatitudeText;
     private TextView mLongitudeText;
-
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     private Button btnCapture;
     private TextureView textureView;
@@ -90,8 +102,8 @@ public class cameraActivity extends AppCompatActivity {
     TextView textView1;
 
     //    String LOCATION_PROVIDER = LocationManager.NETWORK_PROVIDER;
-//    LocationManager locationManager;
-    LocationListener locationListener;
+
+//    LocationListener locationListener;
 
 
     //Check state orientation of output image
@@ -118,6 +130,7 @@ public class cameraActivity extends AppCompatActivity {
     private Handler mBackgroundHandler;
     private HandlerThread mBackgroundThread;
 
+
     CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(@NonNull CameraDevice camera) {
@@ -127,12 +140,16 @@ public class cameraActivity extends AppCompatActivity {
 
         @Override
         public void onDisconnected(@NonNull CameraDevice cameraDevice) {
-            cameraDevice.close();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                cameraDevice.close();
+            }
         }
 
         @Override
         public void onError(@NonNull CameraDevice cameraDevice, int i) {
-            cameraDevice.close();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                cameraDevice.close();
+            }
             cameraDevice = null;
         }
     };
@@ -143,12 +160,14 @@ public class cameraActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+
         final TextView textView = findViewById(R.id.tvTimeStamp);
         textView.setText(timestamp());
         //textView1.setText(hereLocation());
+//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        mLatitudeLabel = getResources().getString(R.string.latitude_label);
-        mLongitudeLabel = getResources().getString(R.string.longitude_label);
+
         mLatitudeText = (TextView) findViewById((R.id.latitude_text));
         mLongitudeText = (TextView) findViewById((R.id.longitude_text));
 
@@ -158,11 +177,81 @@ public class cameraActivity extends AppCompatActivity {
         //From Java 1.4 , you can use keyword 'assert' to check expression true or false
         assert textureView != null;
         textureView.setSurfaceTextureListener(textureListener);
+//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
         btnCapture = (Button) findViewById(R.id.btnCapture);
         btnCapture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 textView.setText(timestamp());
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                // Define the criteria how to select the locatioin provider -> use
+                // default
+                Criteria criteria = new Criteria();
+                String provider = locationManager.getBestProvider(criteria, false);
+                Location location = locationManager.getLastKnownLocation(provider);
+
+                // Initialize the location fields
+                if (location != null) {
+                    mLatitudeText.setText("Provider " + provider + " has been selected.");
+                    onLocationChanged(location);
+                } else {
+                    mLatitudeText.setText("Location not available");
+                    mLongitudeText.setText("Location not available");
+                }
+
+
+
+//                locationListener = new LocationListener() {
+//                    @Override
+//                    public void onLocationChanged(Location location) {
+//                        Log.i("LOC", location.toString());
+//                        Toast.makeText(cameraActivity.this,location.toString(),Toast.LENGTH_LONG).show();
+//                        mLatitudeText.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+////                        mLatitudeText.setText(location.getLatitude());
+////                        mLongitudeText.setText( location.getLongitude());
+////                        String myLoc = new LatLng(location.getLatitude(), location.getLongitude());
+//                    }
+//
+//                    @Override
+//                    public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onProviderEnabled(String provider) {
+//                        Log.i("LOC", provider.toString());
+//                        Toast.makeText(cameraActivity.this,provider.toString(),Toast.LENGTH_LONG).show();
+////
+//                    }
+//
+//                    @Override
+//                    public void onProviderDisabled(String provider) {
+//
+//                    }
+//                };
+
+
+
+//                Location location = appLocationService.getLocation(LocationManager.GPS_PROVIDER);
+
+
+                //you can hard-code the lat & long if you have issues with getting it
+                //remove the below if-condition and use the following couple of lines
+                //double latitude = 37.422005;
+                //double longitude = -122.084095
+
+//                if (location != null) {
+//                    double latitude = location.getLatitude();
+//                    double longitude = location.getLongitude();
+//                    LocationAddress locationAddress = new LocationAddress();
+//                    locationAddress.getAddressFromLocation(latitude, longitude,
+//                            getApplicationContext(), new GeocoderHandler());
+//                } else {
+//                    showSettingsAlert();
+//                }
+
+
                 takePicture();
 
             }
@@ -171,19 +260,27 @@ public class cameraActivity extends AppCompatActivity {
 
     }
 
-
     private String timestamp() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String dateTime = sdf.format(Calendar.getInstance().getTime()); // reading local time in the system
         return (dateTime);
     }
-
+    void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5, this);
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+    @SuppressLint("NewApi")
     private void takePicture() {
         if (cameraDevice == null)
             return;
-        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        @SuppressLint("NewApi") CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
-            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraDevice.getId());
+            @SuppressLint("NewApi") CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraDevice.getId());
             Size[] jpegSizes = null;
             if (characteristics != null)
                 jpegSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
@@ -196,12 +293,12 @@ public class cameraActivity extends AppCompatActivity {
                 width = jpegSizes[0].getWidth();
                 height = jpegSizes[0].getHeight();
             }
-            final ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+            @SuppressLint("NewApi") final ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
             List<Surface> outputSurface = new ArrayList<>(2);
             outputSurface.add(reader.getSurface());
             outputSurface.add(new Surface(textureView.getSurfaceTexture()));
 
-            final CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            @SuppressLint("NewApi") final CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
             captureBuilder.addTarget(reader.getSurface());
             captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
 
@@ -210,13 +307,14 @@ public class cameraActivity extends AppCompatActivity {
             captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
 
             file = new File(Environment.getExternalStorageDirectory() + "/" + UUID.randomUUID().toString() + ".jpg");
-            ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
+            @SuppressLint("NewApi") ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
+                @SuppressLint("NewApi")
                 @Override
                 public void onImageAvailable(ImageReader imageReader) {
                     Image image = null;
                     try {
                         image = reader.acquireLatestImage();
-                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                        @SuppressLint("NewApi") ByteBuffer buffer = image.getPlanes()[0].getBuffer();
                         byte[] bytes = new byte[buffer.capacity()];
                         buffer.get(bytes);
                         save(bytes);
@@ -246,7 +344,7 @@ public class cameraActivity extends AppCompatActivity {
             };
 
             reader.setOnImageAvailableListener(readerListener, mBackgroundHandler);
-            final CameraCaptureSession.CaptureCallback captureListener = new CameraCaptureSession.CaptureCallback() {
+            @SuppressLint("NewApi") final CameraCaptureSession.CaptureCallback captureListener = new CameraCaptureSession.CaptureCallback() {
                 @Override
                 public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
                     super.onCaptureCompleted(session, request, result);
@@ -256,11 +354,12 @@ public class cameraActivity extends AppCompatActivity {
             };
 
             cameraDevice.createCaptureSession(outputSurface, new CameraCaptureSession.StateCallback() {
+                @SuppressLint("NewApi")
                 @Override
                 public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
                     try {
                         cameraCaptureSession.capture(captureBuilder.build(), captureListener, mBackgroundHandler);
-                    } catch (CameraAccessException e) {
+                    } catch (@SuppressLint("NewApi") CameraAccessException e) {
                         e.printStackTrace();
                     }
                 }
@@ -272,11 +371,12 @@ public class cameraActivity extends AppCompatActivity {
             }, mBackgroundHandler);
 
 
-        } catch (CameraAccessException e) {
+        } catch (@SuppressLint("NewApi") CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
+    @SuppressLint("NewApi")
     private void createCameraPreview() {
         try {
             SurfaceTexture texture = textureView.getSurfaceTexture();
@@ -299,29 +399,31 @@ public class cameraActivity extends AppCompatActivity {
                     Toast.makeText(cameraActivity.this, "Changed", Toast.LENGTH_SHORT).show();
                 }
             }, null);
-        } catch (CameraAccessException e) {
+        } catch (@SuppressLint("NewApi") CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
+    @SuppressLint("NewApi")
     private void updatePreview() {
         if (cameraDevice == null)
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
         captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
         try {
             cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(), null, mBackgroundHandler);
-        } catch (CameraAccessException e) {
+        } catch (@SuppressLint("NewApi") CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
 
+    @SuppressLint("NewApi")
     private void openCamera() {
-        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        @SuppressLint("NewApi") CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
             cameraId = manager.getCameraIdList()[0];
-            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
-            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            @SuppressLint("NewApi") CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+            @SuppressLint("NewApi") StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
             assert map != null;
             imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
             //Check realtime permission if run higher API 23
@@ -334,7 +436,7 @@ public class cameraActivity extends AppCompatActivity {
             }
             manager.openCamera(cameraId, stateCallback, null);
 
-        } catch (CameraAccessException e) {
+        } catch (@SuppressLint("NewApi") CameraAccessException e) {
             e.printStackTrace();
         }
     }
@@ -369,6 +471,13 @@ public class cameraActivity extends AppCompatActivity {
                 finish();
             }
         }
+        if (requestCode == 1){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults.length > 0){
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                }
+            }
+        }
 
     }
 
@@ -392,6 +501,7 @@ public class cameraActivity extends AppCompatActivity {
         super.onPause();
     }
 
+    @SuppressLint("NewApi")
     private void stopBackgroundThread() {
         mBackgroundThread.quitSafely();
         try {
@@ -407,6 +517,30 @@ public class cameraActivity extends AppCompatActivity {
         mBackgroundThread = new HandlerThread("Camera Background");
         mBackgroundThread.start();
         mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("LOC", location.toString());
+        Toast.makeText(cameraActivity.this,location.toString(),Toast.LENGTH_LONG).show();
+        mLatitudeText.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
 
